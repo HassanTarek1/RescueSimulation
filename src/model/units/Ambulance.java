f4f85f52cbd4f0f4e102a7ca9b6ab4f8961ab746
HassanTarek1 <47870697+HassanTarek1@users.noreply.github.com>
2019-02-24 19:26:57 +0200

    عملت rename مش اكتر

--- src/model/units/Ambulance.java
+++ src/model/units/Ambulance.java
@@ -1,9 +1,35 @@
 package model.units;
 
-public class Ambulance {
-
+public class Ambulance extends MedicalUnit{
+	private String id;
+	private Address location;
+	private int stepsPerCycle;
+	
 	public Ambulance() {
 		// TODO Auto-generated constructor stub
+	}
+	Ambulance(String id, Address location, int stepsPerCycle){
+		this.id=id;
+		this.location=location;
+		this.stepsPerCycle=stepsPerCycle;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Address getLocation() {
+		return location;
+	}
+	public void setLocation(Address location) {
+		this.location = location;
+	}
+	public int getStepsPerCycle() {
+		return stepsPerCycle;
+	}
+	public void setStepsPerCycle(int stepsPerCycle) {
+		this.stepsPerCycle = stepsPerCycle;
 	}
 
 }
--- src/model/units/DiseaseControlUnit.java
+++ src/model/units/DiseaseControlUnit.java
@@ -1,5 +1,33 @@
 package model.units;
 
-public class DiseaseControlUnit {
-
+public class DiseaseControlUnit extends MedicalUnit{
+	private String id;
+	private Address location;
+	private int stepsPerCycle;
+	public DiseaseControlUnit() {
+		
+	}
+	DiseaseControlUnit(String id, Address location, int stepsPerCycle){
+		this.id=id;
+		this.location=location;
+		this.stepsPerCycle=stepsPerCycle;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Address getLocation() {
+		return location;
+	}
+	public void setLocation(Address location) {
+		this.location = location;
+	}
+	public int getStepsPerCycle() {
+		return stepsPerCycle;
+	}
+	public void setStepsPerCycle(int stepsPerCycle) {
+		this.stepsPerCycle = stepsPerCycle;
+	}
 }
