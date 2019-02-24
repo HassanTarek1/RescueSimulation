86b68912815271906b6dfd991f8d0411e17114c6
Ahmed Mokhtar <ahmed.abdelrehim@student.guc.edu.eg>
2019-02-25 00:18:32 +0200

    24(half),25

--- src/simulation/Simulator.java
+++ src/simulation/Simulator.java
@@ -17,19 +17,34 @@ public class Simulator {
 	private ArrayList<Unit> emergencyUnits;
 	private ArrayList<Disaster> plannedDisasters;
 	private ArrayList<Disaster> executedDisasters;
-	private Address[][]world;
+	private Address[][]world = new Address[10][10];
 	public Simulator() {
-	
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				world[i][j] = new Address(i, j);
+			}
+		}
 	}
-		private String[] readFile(String path) throws IOException{
-		String currentLine = "";
-		String res="";
-		FileReader fileReader= new FileReader(path);
+	
+	private void loadBuildings(String filePath) throws IOException{
+		String currline = "";
+		FileReader fileReader= new FileReader(filePath);
 		BufferedReader br = new BufferedReader(fileReader);
-		while ((currentLine = br.readLine()) != null) {
-		res=res+","+currentLine;
-		// Parsing the currentLine String
+		while ((currline = br.readLine()) != null) {
+			String[] values = currline.split(",");
+			Address a = new Address(Integer.parseInt(values[0]),Integer.parseInt(values[1]));
+			buildings.add(new ResidentialBuilding(a));
 		}
-		return res.split(",");
+	}
+	
+	private void loadCitizens(String filePath) throws IOException{
+		String currline = "";
+		FileReader fileReader= new FileReader(filePath);
+		BufferedReader br = new BufferedReader(fileReader);
+		while ((currline = br.readLine()) != null) {
+			String[] values = currline.split(",");
+			Address a = new Address(Integer.parseInt(values[0]),Integer.parseInt(values[1]));
+			citizens.add(new Citizen(a, values[2], values[3], Integer.parseInt(values[4])));
 		}
+	}
 }
