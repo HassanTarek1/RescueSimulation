d064d8992867f171b0559d9adf73e7d43deae717
HassanTarek1 <47870697+HassanTarek1@users.noreply.github.com>
2019-02-27 17:57:00 +0200

    no new

--- src/simulation/Simulator.java
+++ src/simulation/Simulator.java
@@ -69,7 +69,7 @@ public class Simulator {
 			}
 		}
 		}
-	private void loadDisasters(String filePath) throws IOException {
+	private void loadDisasters(String filePath) throws Exception {
 		String currentLine = "";
 		FileReader fileReader= new FileReader(filePath);
 		BufferedReader br = new BufferedReader(fileReader);
