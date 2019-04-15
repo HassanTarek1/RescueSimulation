58e687123710fdf18b83deadba9dc5efbaf34d1f
Ahmed Mokhtar <ahmed.abdelrehim@student.guc.edu.eg>
2019-04-15 23:13:40 +0200

    available units

--- src/controller/CommandCenter.java
+++ src/controller/CommandCenter.java
@@ -5,12 +5,10 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
 import exceptions.BuildingAlreadyCollapsedException;
@@ -19,12 +17,12 @@ import model.events.SOSListener;
 import model.infrastructure.ResidentialBuilding;
 import model.people.Citizen;
 import model.units.Unit;
+import model.units.UnitState;
 import simulation.Rescuable;
 import simulation.Simulator;
 import view.Button;
 import view.GameGUI;
 import view.MainMenu;
-import view.WrappedLabel;
 
 public class CommandCenter implements SOSListener, MouseListener {
 	private Simulator engine;
@@ -41,13 +39,12 @@ public class CommandCenter implements SOSListener, MouseListener {
 		engine = new Simulator(this);
 		visibleBuildings = new ArrayList<ResidentialBuilding>();
 		visibleCitizens = new ArrayList<Citizen>();
-		emergencyUnits = new ArrayList<Unit>();
+		emergencyUnits = engine.getEmergencyUnits();
 		GUI = new MainMenu(this);
 		
 		
 	}
 
-	
 	public Simulator getEngine() {
 		return engine;
 	}
@@ -230,6 +227,33 @@ public class CommandCenter implements SOSListener, MouseListener {
 		}
 		return null;
 	}
-
 	
+	public int countUnits(Class<?> cls,UnitState S) {
+		int ret = 0;
+		for (Unit unit : emergencyUnits) {
+			if(unit.getClass() == cls && unit.getState() == S) {
+				ret++;
+			}
+		}
+		return ret;
+	}
+	
+	public void updateUnitCount() {
+		GUI.getGame().getPanel().getMidArea().getMiddleEast().getTop().getAmbulance()
+		.setText("X "+countUnits(model.units.Ambulance.class, UnitState.IDLE));
+		
+		GUI.getGame().getPanel().getMidArea().getMiddleEast().getTop().getDiseaseControlUnit().
+		setText("X "+countUnits(model.units.DiseaseControlUnit.class, UnitState.IDLE));
+		
+		GUI.getGame().getPanel().getMidArea().getMiddleEast().getTop().getEvacuator().
+		setText("X "+countUnits(model.units.Evacuator.class, UnitState.IDLE));
+		
+		GUI.getGame().getPanel().getMidArea().getMiddleEast().getTop().getFireTruck().
+		setText("X "+countUnits(model.units.FireTruck.class, UnitState.IDLE));
+		
+		GUI.getGame().getPanel().getMidArea().getMiddleEast().getTop().getGasControlUnit().
+		setText("X "+countUnits(model.units.GasControlUnit.class, UnitState.IDLE));
+					
+	}
+
 }
--- src/view/MiddleEastTop.java
+++ src/view/MiddleEastTop.java
@@ -1,24 +1,106 @@
 package view;
 
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.MouseListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JToolTip;
+import javax.swing.SpringLayout.Constraints;
 
-public class MiddleEastTop extends JPanel{
+import controller.CommandCenter;
+import model.units.UnitState;
+
+public class MiddleEastTop extends JPanel {
 	private UnitPanel availableUnits;
-	public MiddleEastTop() {
+	private MouseListener listener;
+	private MouseListener cont;
+	private CommandCenter cont2;
+	private JButton Ambulance;
+	private JButton DiseaseControlUnit;
+	private JButton Evacuator;
+	private JButton FireTruck;
+	private JButton GasControlUnit;
+	public MiddleEastTop(MouseListener listener,MouseListener cont) {
 		super();
-		
+		this.listener = listener;
+		this.cont = cont;
+		this.cont2 = (CommandCenter) cont;
 		setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
 		setSize(500, 675/2);
 		setOpaque(false);
 		
 		//Contents
 		availableUnits = new UnitPanel("icons/Game panel/UnitPanel.png");
+		availableUnits.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.anchor = GridBagConstraints.CENTER;
+		constraints.weightx = 0;
+		constraints.weighty = 0;
+		//Buttons
+		
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		Ambulance = new JButton("X "+cont2.countUnits(model.units.Ambulance.class, UnitState.IDLE));
+		Ambulance.setIcon(new ImageIcon("icons/Game panel/ambulance.png"));
+		Ambulance.setPreferredSize(new Dimension(90, 50));
+		Ambulance.setToolTipText("Ambulance");
+		availableUnits.add(Ambulance,constraints);
+		
+		constraints.gridx = 1;
+		constraints.gridy = 0;
+		DiseaseControlUnit = new JButton("X "+cont2.countUnits(model.units.DiseaseControlUnit.class, UnitState.IDLE));
+		DiseaseControlUnit.setPreferredSize(new Dimension(90, 50));
+		Ambulance.setToolTipText("Ambulance");
+		DiseaseControlUnit.setToolTipText("DiseaseControlUnit");
+		availableUnits.add(DiseaseControlUnit,constraints);
+		
+		constraints.gridx = 2;
+		constraints.gridy = 0;
+		Evacuator = new JButton("X "+cont2.countUnits(model.units.Evacuator.class, UnitState.IDLE));
+		Evacuator.setToolTipText("Evacuator");
+		Evacuator.setPreferredSize(new Dimension(90, 50));
+		availableUnits.add(Evacuator,constraints);
+		
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		FireTruck = new JButton("X "+cont2.countUnits(model.units.FireTruck.class, UnitState.IDLE));
+		FireTruck.setIcon(new ImageIcon("icons/Game panel/fire truck.png"));
+		FireTruck.setToolTipText("Fire truck");
+		FireTruck.setPreferredSize(new Dimension(90, 50));
+		availableUnits.add(FireTruck,constraints);
+		
+		constraints.gridx = 1;
+		constraints.gridy = 1;
+		GasControlUnit = new JButton("X "+cont2.countUnits(model.units.GasControlUnit.class, UnitState.IDLE));
+		GasControlUnit.setToolTipText("Gas control unit");
+		GasControlUnit.setPreferredSize(new Dimension(90, 50));
+		availableUnits.add(GasControlUnit,constraints);
+		
 		add(availableUnits);
 		add(Box.createRigidArea(new Dimension(10,0)));
 		
 	}
+	public JButton getAmbulance() {
+		return Ambulance;
+	}
+	public JButton getDiseaseControlUnit() {
+		return DiseaseControlUnit;
+	}
+	public JButton getEvacuator() {
+		return Evacuator;
+	}
+	public JButton getFireTruck() {
+		return FireTruck;
+	}
+	public JButton getGasControlUnit() {
+		return GasControlUnit;
+	}	
+	
 }
