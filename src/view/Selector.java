c5ba0bf4918d9e54eda71d705ede005e930a9e20
Ahmed Mokhtar <ahmed.abdelrehim@student.guc.edu.eg>
2019-04-17 01:01:33 +0200

    citizen list

--- src/view/Selector.java
+++ src/view/Selector.java
@@ -1,13 +1,21 @@
 package view;
 
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
+import javax.swing.ComboBoxModel;
 import javax.swing.JComboBox;
 
 import model.people.Citizen;
+import simulation.Address;
 
-public class Selector extends JComboBox {
-	public Selector(String[] arr) {
-		super(arr);
+public class Selector extends JComboBox<Citizen> {
+	
+	private MouseListener cont;
+	public Selector(MouseListener cont) {
+		
+		super();
+		this.cont = cont;
 	}
+	
 }
