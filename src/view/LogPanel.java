c1c19db3f89e5b9ca5c549ec6cd9d75d759ee800
Ahmed Mokhtar <ahmed.abdelrehim@student.guc.edu.eg>
2019-04-19 00:12:33 +0200

    GUI updates

--- src/view/LogPanel.java
+++ src/view/LogPanel.java
@@ -3,8 +3,12 @@ package view;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.ScrollPane;
 import java.awt.TextArea;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.Box;
 import javax.swing.JScrollPane;
@@ -15,16 +19,26 @@ import javax.swing.ScrollPaneLayout;
 import javax.swing.border.LineBorder;
 
 public class LogPanel extends JScrollPane{
-private JTextArea textArea;
-	
+	private JTextArea textArea;
+	private File font_file = new File("Fonts/8-Bit Madness.ttf");
+	private Font font;	
+
 	public JTextArea getTextArea() {
 		return textArea;
 	}
 
-	public LogPanel(JTextArea textArea) {
+	public LogPanel(JTextArea textArea) throws FontFormatException, IOException {
 		super(textArea);
 		this.textArea=textArea;
 		this.setSize(textArea.getSize());
+		
+		//font
+		font = Font.createFont(Font.TRUETYPE_FONT, font_file);
+		Font correct8BitFont = font.deriveFont(20f);
+		textArea.setFont(correct8BitFont);
+		
+		textArea.setBackground(Color.decode("#EDBA61"));
+		
 		this.setPreferredSize(this.getSize());
 		setMaximumSize(this.getPreferredSize());
 		this.setLayout(new ScrollPaneLayout());
@@ -32,7 +46,6 @@ private JTextArea textArea;
 		setAlignmentX(Component.CENTER_ALIGNMENT);
 		this.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		this.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		//new
 	}
 
 
--- src/view/TopBar.java
+++ src/view/TopBar.java
@@ -1,9 +1,13 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -12,25 +16,34 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
 public class TopBar extends ImagePanel{
+	private File font_file = new File("Fonts/8-Bit Madness.ttf");
+	private Font font;	
 	private MouseListener listener;
 	private Button muteButton;
 	private Button backButton;
+	private JLabel target;
+	private JLabel unit;
 	private Button endCycle;
 	private Button treat;
 	private MouseListener cont;
 	private JLabel topInfo;
-	public TopBar(String img,MouseListener e,MouseListener cont) {
+	public TopBar(String img,MouseListener e,MouseListener cont) throws FontFormatException, IOException {
 		super(img);
 		listener = e;
 		setSize(1425,60);
 		setLayout(null);
 		this.cont=cont;
-		topInfo=new JLabel();
-		topInfo.setLocation(200, 5);
-		topInfo.setSize(300, 50);
+		
+		//Font
+		font = Font.createFont(Font.TRUETYPE_FONT, font_file);
+		Font correct8BitFont = font.deriveFont(25f);
+	
+		topInfo=new JLabel("",SwingConstants.LEFT);
+		topInfo.setBackground(Color.black);
+		topInfo.setLocation(125, 5);
+		topInfo.setSize(380, 50);
 		topInfo.setVisible(true);
-		topInfo.setFont(new Font("Courier ",Font.BOLD, 16));
-		topInfo.setText("");
+		topInfo.setFont(correct8BitFont);
 		this.add(topInfo);
 		
 		//Buttons
@@ -46,30 +59,54 @@ public class TopBar extends ImagePanel{
 			//Mute
 			muteButton=new Button("icons/Game panel/mute.png");
 			muteButton.addMouseListener(listener);
-			muteButton.setAlignmentX(Component.LEFT_ALIGNMENT);
 			add(Box.createRigidArea(new Dimension(15,0)));
 			add(muteButton);
 			muteButton.setLocation(70,10);
 			muteButton.setSize(40,40);
 			
 			//endCycle
-			endCycle=new Button("icons/Game panel/endCycle.png");
-			//endCycle.addMouseListener(listener);
+			endCycle=new Button("icons/Game panel/Next Cycle.png");
 			endCycle.setAlignmentX(Component.RIGHT_ALIGNMENT);
-			//add(Box.createRigidArea(new Dimension(15,0)));
 			this.add(endCycle);
-			endCycle.setLocation(1200,17);
-			endCycle.setSize(135,25);;
+			endCycle.setLocation(1278,22);
+			endCycle.setSize(135,16);;
 			endCycle.addMouseListener(cont);
 			
 			//treat
-			treat=new Button("icons/Game panel/treat.png");
+			treat=new Button("icons/Game panel/Respond.png");
 			treat.setAlignmentX(Component.RIGHT_ALIGNMENT);
 			add(Box.createRigidArea(new Dimension(15,0)));
 			this.add(treat);
-			treat.setLocation(1100,19);
-			treat.setSize(64,18);;
+			treat.setLocation(1150,19);
+			treat.setSize(135,22);
 			treat.addMouseListener(cont);
+			
+			//Target
+			Font correct8BitFontSmall = font.deriveFont(20f);
+			
+			target=new JLabel("Target: Empty",SwingConstants.RIGHT);
+			target.setBackground(Color.black);
+			target.setLocation(760, -5);
+			target.setSize(380, 50);
+			target.setVisible(true);
+			target.setFont(correct8BitFontSmall);
+			this.add(target);
+			
+			//unit		
+			unit =new JLabel("Unit: Empty",SwingConstants.RIGHT);
+			unit.setBackground(Color.black);
+			unit.setLocation(760, 15);
+			unit.setSize(380, 50);
+			unit.setVisible(true);
+			unit.setFont(correct8BitFontSmall);
+			this.add(unit);
+			
+	}
+	public JLabel getUnit() {
+		return unit;
+	}
+	public JLabel getTarget() {
+		return target;
 	}
 	public Button getMuteButton() {
 		return muteButton;
