c1879340aac461779a9bcef1f57b0165a0a856d6
Ahmed Mokhtar <ahmed.abdelrehim@student.guc.edu.eg>
2019-04-19 00:57:44 +0200

    filled the gap

--- src/view/MidWest.java
+++ src/view/MidWest.java
@@ -19,22 +19,18 @@ public class MidWest extends JPanel{
 		setSize(500, 675);
 		setOpaque(false);
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		add(Box.createRigidArea(new Dimension(0,0)));
 		
 		//Contents
 		Log = new WrappedLabel("", "icons/Game panel/log.png");
 		add(Log);
-		add(Box.createRigidArea(new Dimension(0,0)));
 		
 		top = new MidWestTop("icons/Game panel/InfoPanel.png");
 		add(Box.createRigidArea(new Dimension(10,0)));
 		add(top);
-		add(Box.createRigidArea(new Dimension(0,0)));
 		
 		
 		info = new WrappedLabel("", "icons/Game panel/info.png");
 		add(info);
-		add(Box.createRigidArea(new Dimension(0,0)));
 		
 		bottom = new MidWestBottom("icons/Game panel/InfoPanel.png");
 		add(Box.createRigidArea(new Dimension(10,0)));
